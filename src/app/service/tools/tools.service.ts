bb66949890be6ceb8006201781c257e8769323fc
Adrien RICHARD <richardadrien0@gmail.com>
2019-05-20 20:53:45 +0200

    remouved id prefix

--- src/app/service/tools/tools.service.ts
+++ src/app/service/tools/tools.service.ts
@@ -8,6 +8,6 @@ export class ToolsService {
   constructor() { }
 
   GenerateID() {
-    return '_' + Math.random().toString(36).substr(2, 9);
+    return Math.random().toString(36).substr(2, 9);
   };
 }
