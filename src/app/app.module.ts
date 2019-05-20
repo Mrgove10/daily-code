e127fe9d2ab2ded5d0e5703ac98a20de48c8e1ad
Adrien RICHARD <richardadrien0@gmail.com>
2019-05-20 21:09:14 +0200

    changes app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,28 +8,26 @@ import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 
-// NG Translate
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-
 import { ElectronService } from './providers/electron.service';
 
 import { WebviewDirective } from './directives/webview.directive';
 
+// NG Translate
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+//pages
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { SettingsComponent } from './components/settings/settings.component';
 import { CalendarComponent } from './components/calendar/calendar.component';
 import { EditorComponent } from './components/editor/editor.component';
-import { LMarkdownEditorModule } from 'ngx-markdown-editor';
 import { HelpComponent } from './components/help/help.component';
 
-/*
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons';
-*/
+//markdown editor
+import { LMarkdownEditorModule } from 'ngx-markdown-editor';
+
+
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
